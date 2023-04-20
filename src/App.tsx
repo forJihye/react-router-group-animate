c08fa9fcd741cd28b130ff5c2af726f84f202b3d
JIHYE PARK <36693976+forJihye@users.noreply.github.com>
2023-04-20 05:42:09 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,23 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import React from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { state } from "./store";
 import Page3 from "./pages/Page3";
 import Page2 from "./pages/Page2";
 import Page1 from "./pages/Page1";
-
-const useDirectionHook = () => {
-  const location = useLocation();
-  const { pages } = state;
-  const [currentPath, setCurrentPath] = useState("/");
-  const [direction, setDirection] = useState("right");
-
-  useEffect(() => {
-    setCurrentPath(location.pathname);
-    const prevIndex = pages.findIndex(({ path }) => path === currentPath);
-    const index = pages.findIndex(({ path }) => path === location.pathname);
-    prevIndex <= index ? setDirection("right") : setDirection("left");
-  }, [location.pathname]);
-
-  return [currentPath, direction];
-};
+import useDirectionHook from "./store/history";
 
 function App() {
   const location = useLocation();
-  const [currentPath, direction] = useDirectionHook();
-  // 방향 결과
-  console.log(currentPath, direction);
+  const direction = useDirectionHook();
 
   return (
-    <TransitionGroup id="wrapper">
+    <TransitionGroup
+      id="wrapper"
+      childFactory={(child: any) => {
+        return React.cloneElement(child, { classNames: direction });
+      }}
+    >
       <CSSTransition
         key={location.pathname}
         classNames={direction}
